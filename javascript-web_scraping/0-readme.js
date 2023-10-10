10a097ab2b15ab3d56363f364541cd26e69a449b
khbesbes <5786@holbertonstudents.com>
2023-10-10 16:01:07 +0100

    hello

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readfile(process.argv[2], 'utf-8' ,(err,data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
